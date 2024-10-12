a77d68d4a8ec729a3d1327c44534e6eb46f1bca1
Julie Yu <julie.12.yu@gmail.com>
2024-10-12 16:33:23 -0700

    it works kinda? swipe works :3 with image of car

--- CatTinder/src/App.tsx
+++ CatTinder/src/App.tsx
@@ -1,9 +1,8 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import TinderDeck from './components/TinderDeck';
 import searchPets from './api/SearchPets';
+import React from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,19 +12,9 @@ function App() {
   return (
     <>
       <div>
-        <TinderDeck></TinderDeck>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <div className='app'>
+            <TinderDeck />
+        </div>
         <button onClick={searchPets}>
           Fetch Pet Data
         </button>
@@ -39,9 +28,6 @@ function App() {
             {petData.img && <img src={petData.img} alt={petData.name} />}
           </div>
         )}
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
     </>
   );
