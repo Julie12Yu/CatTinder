3aa0bd08c68199e7e5a9fc49e9c891eb71477131
smallboar <williampfang@gmail.com>
2024-10-12 22:18:43 -0700

    fix: Broken filter API

--- CatTinder/src/components/Tinder/TinderPage.tsx
+++ CatTinder/src/components/Tinder/TinderPage.tsx
@@ -1,29 +1,21 @@
 import './TinderPage.css';
 import React, { useState } from 'react';
 import TinderDeck from './TinderDeck'; // Assuming you have this component
-import searchPets from '../../api/SearchPets';
+import searchPetsWithFilters from '../../api/SearchPetsWithFilters';
+import CatPreference from '../../models/CatPreference';
 
-const TinderPage: React.FC = () => {
+
+interface TinderProfileProps {
+  preferences: CatPreference;
+}
+const TinderPage: React.FC<TinderProfileProps> = (props: TinderProfileProps) => {
   const [petData, setPetData] = useState({ name: "", img: "", id: "" });
 
   return (
     <div>
       <div className="app">
-        <TinderDeck />
+        <TinderDeck preferences={props.preferences}/>
       </div>
-      <button onClick={() => searchPets({ radius: 50, sort: "Updated Date", sortAscending: false })}>
-        Fetch Pet Data
-      </button>
-      {petData.name && (
-        <div id="List">
-          <div>
-            <a href={`https://www.petfinder.com/petdetail/${petData.id}`}>
-              {petData.name}
-            </a>
-          </div>
-          {petData.img && <img src={petData.img} alt={petData.name} />}
-        </div>
-      )}
     </div>
   );
 };
