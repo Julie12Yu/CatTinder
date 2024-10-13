ac7ae30e61475cc6547a6cadd14ad0d588e29601
smallboar <williampfang@gmail.com>
2024-10-13 02:58:17 -0700

    fix: Fix server crash if empty

--- CatTinderBackend/routes.js
+++ CatTinderBackend/routes.js
@@ -11,8 +11,9 @@ router.post('/searchPetsWithFilters', async (req, res) => {
     const cats = await searchPetsWithFilters(props);
     if (cats.length === 0) {
       res.status(400).json({ error: 'No cats found for the current filter.' });
+    } else {
+      res.status(200).json(cats);
     }
-    res.status(200).json(cats);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
